5e238f71eaf58998c30b4687454b604178061424
Preflower <preflower.lin@gmail.com>
2023-05-29 15:05:50 +0800

    chore: add pinia-plugin-persistedstate to auto persist

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
 
 import 'virtual:uno.css'
 
+const pinia = createPinia()
+pinia
+  .use(piniaPluginPersistedstate)
+
 const app = createApp(App)
 
 app
   .use(i18n)
   .use(router)
-  .use(createPinia())
+  .use(pinia)
   .mount('#app')
