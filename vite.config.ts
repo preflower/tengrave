6440994898efdfab21e447785d467140c2ea6ef3
Preflower <preflower.lin@gmail.com>
2024-02-01 15:47:56 +0800

    feat: add analyze command

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,40 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import AutoImport from 'unplugin-auto-import/vite'
-import UnoCSS from 'unocss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: '0.0.0.0'
-  },
-  plugins: [
-    react(),
-    AutoImport({
-      imports: [
-        'react',
-        'react-i18next',
-        'react-router-dom'
-      ],
-      dts: true,
-      eslintrc: {
-        enabled: true // Default `false`
+export default defineConfig(() => {
+  const isAnalyze = process.argv.includes('analyze')
+
+  return {
+    server: {
+      host: '0.0.0.0'
+    },
+    plugins: [
+      react(),
+      AutoImport({
+        imports: [
+          'react',
+          'react-i18next',
+          'react-router-dom'
+        ],
+        dts: true,
+        eslintrc: {
+          enabled: true // Default `false`
+        }
+      }),
+      isAnalyze
+        ? visualizer({
+          filename: path.resolve(__dirname, 'node_modules/rollup-plugin-visualizer/stats.html'),
+          open: true
+        })
+        : undefined
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
       }
-    }),
-    UnoCSS()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
     }
   }
 })
